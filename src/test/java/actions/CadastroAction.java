af0485cf2303a4ba92fc1cba97db60b137b5f21d
Diego Quirino <diego.quirino95@hotmail.com>
2020-10-21 00:24:30 -0300

    steps

--- src/test/java/actions/CadastroAction.java
+++ src/test/java/actions/CadastroAction.java
@@ -1,12 +1,14 @@
 package actions;
 
+import org.junit.Assert;
+
 import io.restassured.response.Response;
 import request.RequestApi;
 
 public class CadastroAction {
 
 	RequestApi request = new RequestApi();
-	Response response = null;
+    static Response response = null;
 	
 	public void realizarChamadaPostApi(String json) {
 		response = request.postApiCadastroUser(json);
@@ -15,6 +17,24 @@ public class CadastroAction {
 	public void validarCadastroPositivo() {
 		System.out.println(response.body().asString());
 		response.then().statusCode(201);
+	
+	}
+	
+	public void validarCadastroNegativoCpf() {
+		response.then().statusCode(400);
+		String erro = response.then().extract().path("erro").toString();
+		if (!erro.contains("Já existe pessoa cadastrada com o CPF")) {
+			Assert.assertFalse(false);
+		}
+	
+	}
+	
+	public void validarCadastroNegativoTelefone() {
+		response.then().statusCode(400);
+		String erro = response.then().extract().path("erro").toString();
+		if (!erro.contains("Já existe pessoa cadastrada com o Telefone")) {
+			Assert.assertFalse(false);
+		}
 	
 	}
 	
--- target/test-classes/features/Cadastro.feature
+++ target/test-classes/features/Cadastro.feature
@@ -12,13 +12,13 @@ Cenário: Cadastrar um usuário no sistema
 Cenário: Cadastrar um usuário no sistema com cpf já cadastro
     Dado que estou fazendo o cadastro de uma pessoa
     Quando insiro os dados do "cadastro2"
-    Então Valido que o cadastrado foi realizado
+    Então Valido que o cadastrado não foi realizado por duplicidade no cpf
     
  @cadastroNegativoTelefone
 Cenário: Cadastrar um usuário no sistema com telefone já cadastrado
     Dado que estou fazendo o cadastro de uma pessoa
     Quando insiro os dados do "cadastro3"
-    Então Valido que o cadastrado foi realizado
+    Então Valido que o cadastrado não foi realizado duplicidade no telefone
    
     
   
